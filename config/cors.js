90dcbf5397c498cde54d743e4689aaec61949758
miracleibanze2 <miracleibanze@gmail.com>
2024-12-26 19:46:55 +0200

    correcting address

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.ORIGIN,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
